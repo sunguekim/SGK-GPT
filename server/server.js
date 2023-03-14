fa44d847ab1f992ea0bba8eab29cc09263ff4481
sunguekim <sunguekim96@gmail.com>
2023-03-14 09:21:08 +0900

    add__프로젝트 시작 폴더와 간단한 서버코드만 추가했습니다

---
+++ server/server.js
@@ -0,0 +1,44 @@
+import express from 'express'
+import * as dotenv from "dotenv";
+import cors from "cors";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.status(200).send({
+    message: "API 테스트입니다!",
+  });
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+
+    const response = await openai.createCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "system", content: prompt }],
+    });
+
+    res.status(200).send({
+      bot: response.data.choices[0].text,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error || "서버 에러입니다");
+  }
+});
+
+app.listen(5000, () =>
+  console.log("AI server started on http://localhost:5000")
+);
